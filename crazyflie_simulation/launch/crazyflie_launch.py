e7eba49dd5a305ff490ab5566800ae98e71d701d
knmcguire <kimberly@bitcraze.io>
2022-11-11 13:20:24 +0100

    added simulation files from experimental github

---
+++ crazyflie_simulation/launch/crazyflie_launch.py
@@ -0,0 +1,48 @@
+import os
+import pathlib
+import launch
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from webots_ros2_driver.webots_launcher import WebotsLauncher
+
+
+def generate_launch_description():
+    package_dir = get_package_share_directory('crazyflie_simulation')
+    robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'crazyflie.urdf')).read_text()
+
+    webots = WebotsLauncher(
+        world=os.path.join(package_dir, 'worlds', 'crazyflie_world.wbt')
+    )
+
+    my_robot_driver = Node(
+        package='webots_ros2_driver',
+        executable='driver',
+        output='screen',
+        parameters=[
+            {'robot_description': robot_description,
+             'use_sim_time': True,
+             'set_robot_state_publisher': True},
+        ]
+    )
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': '<robot name=""><link name=""/></robot>'
+        }],
+    )
+
+    return LaunchDescription([
+        webots,
+        my_robot_driver,
+        robot_state_publisher,
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=webots,
+                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+            )
+        )
+    ])
