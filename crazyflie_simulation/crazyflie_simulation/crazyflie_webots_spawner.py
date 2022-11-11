31dea244313c2ee880b4c191cf03d5cfa1c720b2
knmcguire <kimberly@bitcraze.io>
2022-11-11 16:05:56 +0100

    spawn crazyflies in simulation based on yaml

--- crazyflie_simulation/crazyflie_simulation/crazyflie_webots_spawner.py
+++ crazyflie_simulation/crazyflie_simulation/crazyflie_webots_spawner.py
@@ -1,18 +1,40 @@
 import rclpy
-from geometry_msgs.msg import Twist
-
-HALF_DISTANCE_BETWEEN_WHEELS = 0.045
-WHEEL_RADIUS = 0.025
+from ament_index_python.packages import get_package_share_directory
+import os
+import yaml
 
 class CrazyflieSpawner:
     def init(self, webots_node, properties):
-        self.robot = webots_node.robot
-
         rclpy.init(args=None)
         self.node = rclpy.create_node('crazyflie_spawner')
+
+        # load crazyflies
+        crazyflies_yaml = os.path.join(
+            get_package_share_directory('crazyflie'),
+            'config',
+            'crazyflies.yaml')
+
+        with open(crazyflies_yaml, 'r') as ymlfile:
+            crazyflies_yml = yaml.safe_load(ymlfile)
+
+        robot_data = crazyflies_yml['robots']
+
+        # Create easy lookup tables for uri, name and types
+        self.names = []
+        self.location_dict = {}
+        for crazyflie in robot_data:
+            if robot_data[crazyflie]["enabled"]:
+                self.location_dict[crazyflie]=robot_data[crazyflie]["initial_position"]
+                self.names.append(crazyflie)
+            
+            self.node.get_logger().info(crazyflie)
+
+        self.robot = webots_node.robot
         root_node = self.robot.getRoot()
         children_field = root_node.getField('children')
-        children_field.importMFNodeFromString(-1, 'Crazyflie {  translation 0 1 -0.05 name "crazyflie"  controller "<extern>"}')
+        for name in self.names:
+            location = self.location_dict[name]
+            children_field.importMFNodeFromString(-1, 'Crazyflie {  translation '+str(location[0])+' '+ str(location[1])+' '+str(location[2])+' name "'+ name +'"  controller "<extern>"}')
 
     def step(self):
         rclpy.spin_once(self.node, timeout_sec=0)
--- crazyflie_simulation/launch/crazyflie_launch.py
+++ crazyflie_simulation/launch/crazyflie_launch.py
@@ -6,11 +6,29 @@ from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher, Ros2SupervisorLauncher
 from webots_ros2_driver.urdf_spawner import URDFSpawner, get_webots_driver_node
+import yaml
 
 def generate_launch_description():
     package_dir = get_package_share_directory('crazyflie_simulation')
     robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'crazyflie.urdf')).read_text()
     supervisor_description = pathlib.Path(os.path.join(package_dir, 'resource', 'supervisor.urdf')).read_text()
+
+    # load crazyflies
+    crazyflies_yaml = os.path.join(
+        get_package_share_directory('crazyflie'),
+        'config',
+        'crazyflies.yaml')
+
+    with open(crazyflies_yaml, 'r') as ymlfile:
+        crazyflies_yml = yaml.safe_load(ymlfile)
+
+    robot_data = crazyflies_yml['robots']
+
+    # Create easy lookup tables for uri, name and types
+    names = []
+    for crazyflie in robot_data:
+        if robot_data[crazyflie]["enabled"]:
+            names.append(crazyflie)
 
     webots = WebotsLauncher(
         world=os.path.join(package_dir, 'worlds', 'crazyflie_world.wbt')
@@ -23,29 +41,32 @@ def generate_launch_description():
         additional_env={'WEBOTS_CONTROLLER_URL': 'Ros2Supervisor'},
         respawn=True,
         parameters=[
-            {'robot_description': supervisor_description}
+            {'robot_description': supervisor_description},
         ]
     )
+    ld = LaunchDescription()
+    ld.add_action(webots)
+    ld.add_action(supervisor)
 
-    my_robot_driver = Node(
-        package='webots_ros2_driver',
-        executable='driver',
-        output='screen',
-        additional_env={'WEBOTS_CONTROLLER_URL': 'crazyflie'},
-        parameters=[
-            {'robot_description': robot_description}
-        ]
-    )
-
-
-    return LaunchDescription([
-        webots,
-        my_robot_driver,
-        supervisor,
-        launch.actions.RegisterEventHandler(
+    for name in names:
+        crazyflie_driver = Node(
+            package='webots_ros2_driver',
+            executable='driver',
+            output='screen',
+            additional_env={'WEBOTS_CONTROLLER_URL': name},
+            parameters=[
+                {'robot_description': robot_description}
+            ]
+        )
+        ld.add_action(crazyflie_driver)
+        ld.add_action(launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=webots,
                 on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
             )
-        )
-    ])
+        ))
+
+
+
+
+    return ld
